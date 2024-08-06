67e4d02c9cb8d404e016bbfe86e0d66ef21b10ef
LeandroF01 <leaylio3327@gmail.com>
2024-08-06 18:01:30 -0300

    Add  Auth to controllers

---
+++ OrderManagementDB_API/Controllers/AuthController .cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderManagementDB_API.Models;
+using OrderManagementDB_API.Services;
+
+namespace OrderManagementDB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            var token = await _authService.AuthenticateAsync(loginModel);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new { Token = token });
+        }
+    }
+}
