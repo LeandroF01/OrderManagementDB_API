9b9e0ad36ba54f7c0fc31342098fb88a1e91eec2
LeandroF01 <leaylio3327@gmail.com>
2024-08-14 20:05:11 -0300

    Add controllers to ProductIngredients and Ingredients

---
+++ OrderManagementDB_API/Controllers/IngredientsController.cs
@@ -0,0 +1,85 @@
+﻿using DB;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderManagementDB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+
+        private OrderManagementDBContext _context;
+
+        public IngredientsController(OrderManagementDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ingredients
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ingredients>>> Get()
+        {
+            return await _context.Ingredients.ToListAsync();
+        }
+
+        // GET: api/ingredients/5
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ingredients>> Get(int id)
+        {
+            var ingredients = await _context.Ingredients.FindAsync(id);
+            if (ingredients == null)
+            {
+                return NotFound();
+            }
+            return ingredients;
+        }
+
+        // POST: api/ingredients
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult<Ingredients>> Post(Ingredients ingredients)
+        {
+            _context.Ingredients.Add(ingredients);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = ingredients.IngredientID }, ingredients);
+        }
+
+        // PUT: api/ingredients/5
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Ingredients ingredients)
+        {
+            if (id != ingredients.IngredientID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ingredients).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/ingredients/5
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ingredients = await _context.Ingredients.FindAsync(id);
+            if (ingredients == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ingredients.Remove(ingredients);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ OrderManagementDB_API/Controllers/ProductIngredientsController.cs
@@ -0,0 +1,84 @@
+﻿using DB;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderManagementDB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductIngredientsController : ControllerBase
+    {
+        private OrderManagementDBContext _context;
+
+        public ProductIngredientsController(OrderManagementDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/productIngredients
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductIngredients>>> Get()
+        {
+            return await _context.ProductIngredients.ToListAsync();
+        }
+
+        // GET: api/productIngredients/5
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductIngredients>> Get(int id)
+        {
+            var productIngredients = await _context.ProductIngredients.FindAsync(id);
+            if (productIngredients == null)
+            {
+                return NotFound();
+            }
+            return productIngredients;
+        }
+
+        // POST: api/productIngredients
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult<ProductIngredients>> Post(ProductIngredients productIngredients)
+        {
+            _context.ProductIngredients.Add(productIngredients);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = productIngredients.ProductIngredientID }, productIngredients);
+        }
+
+        // PUT: api/productIngredients/5
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, ProductIngredients productIngredients)
+        {
+            if (id != productIngredients.ProductIngredientID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productIngredients).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/productIngredients/5
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var productIngredients = await _context.ProductIngredients.FindAsync(id);
+            if (productIngredients == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductIngredients.Remove(productIngredients);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
