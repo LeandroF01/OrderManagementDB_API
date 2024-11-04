7ec5ab08a9736115c514988ccd457e8a9b6d37fb
LeandroF01 <leaylio3327@gmail.com>
2024-11-04 10:01:16 -0300

    New Tables Model

---
+++ DB/Tables.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    internal class Tables
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TableID { get; set; }
+
+        [Required]
+        public int TableNumber { get; set; }
+
+        [Required]
+        public float PositionX { get; set; }
+
+        [Required]
+        public float PositionY { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Status { get; set; } = "Available"; // Default value
+
+        [Required]
+        public int Capacity { get; set; }
+
+        // Navigation property to Orders
+        public virtual ICollection<Orders> Orders { get; set; } = new HashSet<Orders>();
+
+    }
+}
